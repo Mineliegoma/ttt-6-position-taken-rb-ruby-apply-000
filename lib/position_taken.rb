7fbe4a402507f3c143fd4895e489f1b9d4b2cd74
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-23 01:27:57 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -3,15 +3,14 @@
 def position_taken?( board, index)
 
     
-    if board[index] == "X" || board[index] == "0" || board[index] == nil
-  false
+    if board[index] == " " || board[index] == "" || board[index] == nil
+     false
 
     
    #if board[index] = " " || board[index] = ""
     #true
   else 
-    
-true
+  true
   
 end
 end
