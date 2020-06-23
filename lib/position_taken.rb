fd2ec58cbebf7c2a0930d71064b24a12f284dea8
adept-bell-4492 <garlandegoma@gmail.com>
2020-06-23 01:19:28 +0000

    Automatically backed up by Learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,6 +1,8 @@
 # code your #position_taken? method here
 
 def position_taken?( board, index)
+  
+  
   # if board["x"] == 0 || board["0"] == 0
    # elsif  board.index ==" " || board.index == ""
    # false
@@ -8,31 +10,15 @@ def position_taken?( board, index)
    #true
   #else
     #false
+    
+    if board[index] = "X" || board[index] = "0"
+  true
+end
+    
    if board[index] = " " || board[index] = ""
-    false
-  else 
-    #board[index] != "X" || board[index] != "0"
-  truelearn
-  true
-
-  end
-  def position_taken?( board, index)
-  # if board["x"] == 0 || board["0"] == 0
-   # elsif  board.index ==" " || board.index == ""
-   # false
-   # elsif board.index  == "X" || board.index =="O"
-   #true
-  #else
-    #false
-    
-    board[index] = "X" || board[index] = "0"
-  true
-    
-   if board[index] != " " || board[index] != ""
     true
   else 
     
 
   end
-end
 end
